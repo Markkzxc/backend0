32fc5789b65ee5acf1fc648cf29485dd1e1a8d8a
Markkzxc <tenio.markdave@cec.edu.ph>
2025-11-26 12:17:09 +0800

    first commitadasdas

--- firebase.js
+++ firebase.js
@@ -1,16 +1,12 @@
 // firebase.js
 import admin from "firebase-admin";
+// Import the service account JSON
+import serviceAccount from "./serviceAccountKey.json" assert { type: "json" };
 
-// Make sure you have your service account stored in an environment variable
-if (!process.env.GOOGLE_SERVICE_ACCOUNT) {
-  throw new Error("Missing GOOGLE_SERVICE_ACCOUNT environment variable!");
-}
-
-// Parse the JSON string from the env variable
-const serviceAccount = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT);
-
+// Initialize Firebase Admin with the service account
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-export default admin;
+// Export Firestore instance
+export const db = admin.firestore();
